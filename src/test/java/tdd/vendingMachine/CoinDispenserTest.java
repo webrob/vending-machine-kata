d6e197d8aa742105e45ebe07fa5684b03717596b
webrob <robert.rozmus90@gmail.com>
2015-07-18 21:28:43 +0200

    update CoinDispenserTest to fit given-when-then pattern

--- src/test/java/tdd/vendingMachine/CoinDispenserTest.java
+++ src/test/java/tdd/vendingMachine/CoinDispenserTest.java
@@ -25,72 +25,91 @@ public class CoinDispenserTest
     @Test
     public void shouldReturnCoinsAfterCanceling()
     {
+	//given
 	CoinDispenser dispenser = new CoinDispenser();
 	dispenser.setPrice(new Price(300));
-
 	dispenser.insertCoin(Coin.CENT_20);
 	dispenser.insertCoin(Coin.CENT_50);
-
 	dispenser.returnCoins();
 
+	//when
 	List<Coin> coins = dispenser.takeChange();
 	Price valueOfCoins = CoinHelper.calculateValueOfCoins(coins);
 
+	//then
 	assertThat(valueOfCoins).isEqualTo(new Price(70));
     }
 
     @Test(expected = PriceNotSetRuntimeException.class)
     public void shouldThrowExceptionAfterInsertingCoinWhenPriceIsNotSet()
     {
+	//given
 	CoinDispenser dispenser = new CoinDispenser();
+
+	//when
 	dispenser.insertCoin(Coin.CENT_20);
     }
 
     @Test
     public void shouldReturnChange()
     {
+	//given
 	CoinDispenser dispenser = new CoinDispenser();
 	dispenser.setPrice(new Price(250));
 	dispenser.insertCoin(Coin.PLN_2);
 	dispenser.insertCoin(Coin.PLN_2);
 
+	//when
 	List<Coin> coins = dispenser.takeChange();
 	Price valueOfCoins = CoinHelper.calculateValueOfCoins(coins);
 
+	//then
 	assertThat(valueOfCoins).isEqualTo(new Price(150));
     }
 
     @Test
     public void shouldNotifyListenerWhenChangeIsPrepared()
     {
+	//given
 	CoinDispenser dispenser = new CoinDispenser();
 	dispenser.setInsertingCoinsListener(insertingCoinsListener);
 	dispenser.setPrice(new Price(250));
+
+	//when
 	dispenser.insertCoin(Coin.PLN_2);
 	dispenser.insertCoin(Coin.PLN_2);
 
+	//then
 	verify(insertingCoinsListener).prepareProduct();
     }
 
     @Test
     public void shouldNotifyListenerWhenMoreCoinsAreRequired()
     {
+	//given
 	CoinDispenser dispenser = new CoinDispenser();
 	dispenser.setInsertingCoinsListener(insertingCoinsListener);
 	dispenser.setPrice(new Price(250));
+
+	//when
 	dispenser.insertCoin(Coin.PLN_2);
 
+	//then
 	verify(insertingCoinsListener).insertMoreCoins(new Price(50));
     }
 
     @Test
     public void shouldNotifyListenerWhenChangeIsNotAvailable()
     {
+	//given
 	CoinDispenser dispenser = new CoinDispenser();
 	dispenser.setInsertingCoinsListener(insertingCoinsListener);
 	dispenser.setPrice(new Price(110));
+
+	//when
 	dispenser.insertCoin(Coin.PLN_2);
 
+	//then
 	verify(insertingCoinsListener).changeNotAvailable();
     }
 }
